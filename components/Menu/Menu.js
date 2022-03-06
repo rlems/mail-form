76892e94e18cb7f8a232900fef4f4483fc9cc977
René Lems <rene@ijsfontein.nl>
2022-03-06 17:28:57 +0100

    Little piece of menu animation

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,12 +1,45 @@
 import styles from './Menu.module.scss';
 import {useState} from "react";
 import cn from "classnames";
+import {animated, useTrail, config} from "react-spring";
+
+
+function SideMenuBackground({ onComplete }) {
+  const trail = useTrail(3, {
+    config: config.slow,
+    scale: 1.7,
+    translate: "-50%, 0%",
+    from: {
+      scale: 0,
+      translate: "60%, 0%",
+    },
+    onRest: onComplete
+  });
+
+  return (
+    <div className={styles.bg}>
+      {trail.map((componentStyles, index) => (
+        <animated.div
+          key={index}
+          className={styles.backgroundPart}
+          style={componentStyles}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default function Menu() {
   const [showMenu, setShowMenu] = useState(false);
+  const [showMenuItems, setShowMenuItems] = useState(false);
 
   function toggleShowMenu() {
     setShowMenu(!showMenu);
+    if (showMenu) setShowMenuItems(false);
+  }
+
+  function handleShowMenuItems() {
+    setShowMenuItems(true)
   }
 
   const menuButtonClassNames = cn(styles.menuButton, {
@@ -45,20 +78,24 @@ export default function Menu() {
           className={sideMenuClassNames}
           tabIndex="-1"
         >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a href="#">Outboard motor</a>
-            </li>
-            <li className="nav-item">
-              <a href="#">Inboard motor</a>
-            </li>
-            <li className="nav-item">
-              <a href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a href="#">Contact</a>
-            </li>
-          </ul>
+          {showMenuItems && (
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a href="#">Outboard motor</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Inboard motor</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          )}
+
+          {showMenu && <SideMenuBackground onComplete={handleShowMenuItems} />}
         </div>
       </div>
     </>
