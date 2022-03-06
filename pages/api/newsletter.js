63b5c54375d6f3cd2a30a03eddf1020b8ff1d019
René Lems <rene@ijsfontein.nl>
2022-03-06 17:43:26 +0100

    use env variables

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -4,11 +4,9 @@ import emailValidator from "../../validators/emailValidator";
 const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 mailchimp.setConfig({
-  apiKey: '0532680f25b59daddbe76ab71b2a52ae-us14',
-  server: 'us14',
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_SERVER,
 });
-
-const MAILCHIMP_NEWSLETTER_LIST_ID = '04addb595e'
 
 function validate(input) {
   return requiredValidator(input.firstName).isValid
@@ -21,7 +19,7 @@ export default async function handler(req, res) {
   try {
     const isValid = validate(req.body);
     if (isValid) {
-      const response = await mailchimp.lists.addListMember(MAILCHIMP_NEWSLETTER_LIST_ID, {
+      const response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_NEWSLETTER_LIST_ID, {
         email_address: req.body.email,
         status: "subscribed",
       });
